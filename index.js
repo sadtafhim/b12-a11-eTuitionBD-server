e836f94797b5cdadca77beca507d1835204cd935
Saad <tafhim.sad@gmail.com>
2025-12-06 18:59:44 +0600

    server setup complete

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+var cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
