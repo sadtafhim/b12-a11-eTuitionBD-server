b0adc6f057591bca29cc66cc828f3f4ca5f43f64
Saad <tafhim.sad@gmail.com>
2025-12-16 00:10:22 +0600

    path secured

--- index.js
+++ index.js
@@ -10,7 +10,14 @@ const admin = require("firebase-admin");
 const serviceAccount = require("./e-tuition-bd-firebase-adminsdk.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+  }),
 });
 
 app.use(cors());
